de36e271dec00a86b117fb9ef8e5d6b4d52b4166
CocoMarck <76503881+CocoMarck@users.noreply.github.com>
2023-05-20 17:54:24 -0600

    Add files via upload

--- Modulo_YandexDisk.py
+++ Modulo_YandexDisk.py
@@ -46,43 +46,94 @@ def sync(opc=''):
 
     return sync
     
-def setPath(dir_YD=''):
+def setPath(dir_YD=None):
     # Carpeta
-    if dir_YD == '':
-        dir_YD = f'{Util.Path()}Yandex-Disk'
-    else:
-        pass
-    
-    if pathlib.Path(dir_YD).exists():
-        mkdir = f'# La carpeta es "{dir_YD}"'
-    else:
-        dir_YD = f'{Util.Path()}Yandex-Disk'
-        mkdir = f'mkdir {dir_YD} &&'
+    #if dir_YD == None:
+    #    dir_YD = f'{Util.Path()}Yandex-Disk'
+    #else:
+    #    pass
 
-    # Archivo
+    # Si el Archivo existe
     if pathlib.Path(f'{path}/config.cfg').exists():
+        # Leer el archivo config.cfg
         archive = Util.Text_Read(
             file_and_path=f'{path}/config.cfg',
             opc='ModeText'
         )
-    
-        text = ''
+        
+        # Variables a utilizar
+        dir_correct = None  # Para obtener dir del archivo config.cfg
+        stop_force = False  # Para forzar o no el remplzo del dir del config.cfg
+        go = False          # Para indicar que dir_correct es correcto
+        mkdir = ''          # Para indicar si se crea o no una carpeta
+        
+        # Verificar si existe el dir adentro del config.cfg
         for line in archive.split('\n'):
-            if line.startswith('#dir='):
-                text += f'dir={dir_YD}\n'
+            if line.startswith('dir='):
+                dir_correct = line.split('dir=')
+                dir_correct = dir_correct[1]
+
+                if pathlib.Path(dir_correct).exists():
+                    go = True
+                
+                else:
+                    dir_correct = None
+                
+            elif line.startswith('#dir='):
+                dir_YD = f'{Util.Path()}Yandex-Disk'
+                mkdir = f'mkdir {dir_YD} &&'
+                
             else:
-                text += line + '\n'
-    
-        with open(f'Path-Config.txt', 'w') as source_file:
-            source_file.write(text)
-        file_replace = f'cp ./Path-Config.txt "{path}/config.cfg"'
+                pass
+        
+        # Verificar que exista el path asignado por el usuario        
+        # Forzar o no el remplazo del dir adentro del config.cfg
+        if pathlib.Path(str(dir_YD)).exists():
+            mkdir = f'# La carpeta es "{dir_YD}"'
+
+        elif dir_YD == None:
+            if dir_correct == None:
+                dir_YD = f'{Util.Path()}Yandex-Disk'
+                if pathlib.Path(str(dir_YD)).exists():
+                    mkdir = f'# MODO carpeta Default "{dir_YD}"'
+                else:
+                    mkdir = f'mkdir {dir_YD} &&'
+            
+            else:
+                mkdir = f'# La carpeta es "{dir_correct}"'
+                stop_force = True
+        else:
+            pass
+        
+        # Remplazar o no el dir adentro del config.cfg
+        if (
+            dir_correct == dir_YD and go == True or
+            stop_force == True
+        ):
+            file_replace = f'# Ya esta establecido el dir "{dir_correct}"'
+
+        else:
+            text = ''
+            for line in archive.split('\n'):
+                if (
+                    line.startswith('#dir=') or
+                    line.startswith('dir=')
+                ):
+                    text += f'dir={dir_YD}\n'
+                else:
+                    text += line + '\n'
+        
+            with open(f'Path-Config.txt', 'w') as source_file:
+                source_file.write( text[:-1] )
+            file_replace = f'cp ./Path-Config.txt "{path}/config.cfg"'
+
     else:
         file_replace = '# Sin archivo, no se detecto Yandex-Disk'
     
     # Configuracion
     cfg = (
-        '# Carpeta \n' + mkdir + '\n\n'
-        '# Archivo \n' + file_replace
+        '# Carpeta\n' + mkdir + '\n\n'
+        '# Archivo\n' + file_replace
     )
     
     return cfg
